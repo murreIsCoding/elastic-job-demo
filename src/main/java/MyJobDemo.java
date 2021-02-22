5132ea5bcc5c09d8657dfa6e4cab9d8d52deaa12
MurreLau <353765792@qq.com>
2021-02-22 15:25:56 +0800

    Initial commit

---
+++ src/main/java/MyJobDemo.java
@@ -0,0 +1,36 @@
+import org.apache.shardingsphere.elasticjob.api.JobConfiguration;
+import org.apache.shardingsphere.elasticjob.lite.api.bootstrap.impl.ScheduleJobBootstrap;
+import org.apache.shardingsphere.elasticjob.reg.base.CoordinatorRegistryCenter;
+import org.apache.shardingsphere.elasticjob.reg.zookeeper.ZookeeperConfiguration;
+import org.apache.shardingsphere.elasticjob.reg.zookeeper.ZookeeperRegistryCenter;
+
+public class MyJobDemo {
+
+    public static void main(String[] args) {
+        CoordinatorRegistryCenter coordinatorRegistryCenter =createRegistryCenter();
+        new ScheduleJobBootstrap(coordinatorRegistryCenter, new MyJob(), createJobConfiguration("MyJob-0")).schedule();
+        new ScheduleJobBootstrap(coordinatorRegistryCenter, new MyJob(), createJobConfiguration("MyJob-1")).schedule();
+        new ScheduleJobBootstrap(coordinatorRegistryCenter, new MyJob(), createJobConfiguration("MyJob-2")).schedule();
+        new ScheduleJobBootstrap(coordinatorRegistryCenter, new MyJob(), createJobConfiguration("MyJob-3")).schedule();
+        new ScheduleJobBootstrap(coordinatorRegistryCenter, new MyJob(), createJobConfiguration("MyJob-4")).schedule();
+        new ScheduleJobBootstrap(coordinatorRegistryCenter, new MyJob(), createJobConfiguration("MyJob-5")).schedule();
+        new ScheduleJobBootstrap(coordinatorRegistryCenter, new MyJob(), createJobConfiguration("MyJob-6")).schedule();
+        new ScheduleJobBootstrap(coordinatorRegistryCenter, new MyJob(), createJobConfiguration("MyJob-7")).schedule();
+        new ScheduleJobBootstrap(coordinatorRegistryCenter, new MyJob(), createJobConfiguration("MyJob-8")).schedule();
+        new ScheduleJobBootstrap(coordinatorRegistryCenter, new MyJob(), createJobConfiguration("MyJob-9")).schedule();
+
+    }
+
+    private static CoordinatorRegistryCenter createRegistryCenter() {
+        CoordinatorRegistryCenter regCenter = new ZookeeperRegistryCenter(new ZookeeperConfiguration("192.168.11.210:2181", "my-job"));
+        regCenter.init();
+        return regCenter;
+    }
+
+    private static JobConfiguration createJobConfiguration(String jobName) {
+        // 创建作业配置
+        JobConfiguration jobConfig = JobConfiguration.newBuilder(jobName, 10).cron("0/5 * * * * ?").build();
+        return jobConfig;
+    }
+
+}
